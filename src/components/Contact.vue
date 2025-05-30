dfd78dcbc4bb29412e98a41e042de76810db37b2
Tyree Carpenter <carpentertd@vcu.edu>
2025-05-29 23:16:19 -0400

    Fixed contact section

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,3 +1,17 @@
+<script setup>
+function openGitHub() {
+  window.open(
+    "https://github.com/tyreesamurai?tab=repositories",
+    "_blank",
+    "noopener,noreferrer",
+  );
+}
+
+function openEmail() {
+  window.location.href = "mailto:tyree.fullstack@gmail.com";
+}
+</script>
+
 <template>
   <div class="contact" id="contact">
     <h2>Interested in Working Together?</h2>
@@ -5,7 +19,7 @@
       <div class="item">
         <a href="#">Schedule a call</a>
         <p>Let's discuss your project ideas and how I can help you.</p>
-        <button>Schedule a Call</button>
+        <button @click="openEmail">Schedule a Call</button>
       </div>
       <div class="item">
         <a
@@ -15,7 +29,7 @@
           >Explore My Work</a
         >
         <p>View my portfolio to see the kind of work I do for you.</p>
-        <button>View My Work</button>
+        <button @click="openGitHub">View My Work</button>
       </div>
     </div>
   </div>
