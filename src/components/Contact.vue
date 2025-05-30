ab1acfca52f6f56b74391bfe410f5b3c14229405
Tyree Carpenter <carpentertd@vcu.edu>
2025-05-29 23:13:43 -0400

    Quick link fix

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -8,7 +8,12 @@
         <button>Schedule a Call</button>
       </div>
       <div class="item">
-        <a href="#">Explore My Work</a>
+        <a
+          href="https://github.com/tyreesamurai?tab=repositories"
+          target="_blank"
+          rel="noopener noreferrer"
+          >Explore My Work</a
+        >
         <p>View my portfolio to see the kind of work I do for you.</p>
         <button>View My Work</button>
       </div>
